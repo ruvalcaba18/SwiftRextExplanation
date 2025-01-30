5223cbc3fd567a403c2624beaeca52b3da7821b7
JaelRealtor <jael.ruvalcaba@wizeline.com>
2025-01-30 13:07:42 -0800

    Initial Commit

---
+++ SwiftRext/SwiftRextApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftRextApp.swift
+//  SwiftRext
+//
+//  Created by JaelWizeline on 30/01/25.
+//
+
+import SwiftUI
+
+@main
+struct SwiftRextApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
